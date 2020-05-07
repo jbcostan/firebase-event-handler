07007e99cae22f37427a68d50d26b2014f7e608d
jbcostan <35471042+jbcostan@users.noreply.github.com>
2020-05-07 19:20:12 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require("firebase-admin");
+admin.initializeApp();
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+"use strick";
+exports.onMessages = functions.firestore.document('chats/{revMessage}').onWrite((change, context) => {
+    const revMessage = change.after.data();
+    // console.log(`New message ${revMessage}`)
+    if (revMessage) {
+        const revText = revMessage.message;
+        const updatedText = AddChoco(revText);
+        if (revText === updatedText) {
+            console.log("already the same");
+            return null;
+        }
+        return change.after.ref.update({ message: updatedText });
+    }
+    else {
+        return null;
+    }
+});
+function AddChoco(text) {
+    const re = /chocolate/gi;
+    const re2 = /plz/gi;
+    const re3 = /whatz/gi;
+    const re4 = /dudez/gi;
+    const re5 = / u /gi;
+    const re6 = / ur /gi;
+    const re7 = /cheese/gi;
+    const re8 = /WTF/gi;
+    const re9 = /mizz/gi;
+    const re10 = /wuv/gi;
+    return text.replace(re, "\uD83D\uDCA9").replace(re2, "please").replace(re3, "what's")
+        .replace(re4, "my friend").replace(re5, " you ").replace(re6, " your ").replace(re7, "\uD83E\uDDC0")
+        .replace(re8, "why the face").replace(re9, "miss").replace(re10, "\uD83D\uDC96");
+}
+//# sourceMappingURL=index.js.map
